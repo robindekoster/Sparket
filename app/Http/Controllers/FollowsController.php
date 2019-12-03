6d62dc1036fe680fdc7a01768ac4e127bb85b67d
robindekoster <robindekoster2000@hotmail.com>
2019-12-03 01:34:35 +0100

    Follow option partly working

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+Use App\User;
 use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
-    public function store(User)
+    public function store(User $user)
     {
-
+        return $user->username;
     }
 }
